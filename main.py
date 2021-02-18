582b8cf7e5eed751d13c47d1b7cac0e64723eb98
Logan Kirkwood <llk1005@sru.edu>
2021-02-18 15:15:34 -0500

    Changes to main.py

--- main.py
+++ main.py
@@ -5,22 +5,33 @@ def getusername_paswd():
     username = input('Please enter username: ')
     password = input('Please enter password: ')
 
+    pass_user, pass_upper, pass_lower, pass_num, pass_special = False, False, False, False, False
+
+    # tests if username contains @ to make sure it is an email address
+    if "@" in username:
+        pass_user = True
+
     # tests each character in password if one is uppercase or numeric
     for i in password:
-        if i == i.isupper():
+        if i.isupper():
             pass_upper = True
-        if i == i.isnumeric():
+        if i.islower():
+            pass_lower = True
+        if i.isnumeric():
             pass_num = True
 
     # tests if a special character is in password
     if "#" in password or "@" in password or "%" in password or "*" in password:
         pass_special = True
 
-    # Asks user to re-enter their password until it is valid
-    while len(password) > 7 and not(pass_upper) and not(pass_num) and not(pass_special):
-        password = input('Please enter a valid password: ')
+    # calls function again until a valid username and password is entered
+    if pass_user and len(password) > 7 and pass_upper and pass_lower and pass_num and pass_special:
+        print("Valid username and password!")
+        return username, password
 
-    return username, password
+    else:
+        print("Please enter a valid username/password.")
+        getusername_paswd()
 
 
 def secure_store(username, password):
@@ -29,9 +40,15 @@ def secure_store(username, password):
 
 
 def main():
+
+    getusername_paswd()
+
+    """
     susername, spassword = getusername_paswd()
     if susername and spassword:  # Need to change this to check getusername_paswd() returns 1
         secure_store(susername, spassword)
+    """
 
 
-main()
+if __name__ == "__main__":
+    main()
